73e29edaaaa712bb63274603b156eda6dfcddf65
Harsh <harshuttekarweb999@gmail.com>
2026-08-14 14:03:57 +0530

    add all

--- Curd_State/settings.py
+++ Curd_State/settings.py
@@ -83,13 +83,21 @@ WSGI_APPLICATION = 'Curd_State.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
