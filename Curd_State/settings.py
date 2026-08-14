3fa07176433eaf1e871f8b7b54934d8107d90317
Harsh <harshuttekarweb999@gmail.com>
2026-08-14 11:30:00 +0530

    add all

--- Curd_State/settings.py
+++ Curd_State/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-buc63_bd%2cj^%4%eeg2-c6!_gx7it_@ubkv+t%eq4e@8oxfkg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['curd-state.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,3 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
